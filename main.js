b98b99fe3f90bc6d953e481659ff3a8a8fc01658
SmithJoshN <joshsmithbyui@gmail.com>
2020-02-20 16:49:20 -0700

    Updated JS and Text Boxes

--- main.js
+++ main.js
@@ -1,80 +1,37 @@
-const picBox = document.getElementById("pic");
-const wifiBox = document.getElementById("wifi");
-const sidBox = document.getElementById("sid");
-const wpaBox = document.getElementById("wpa");
-
-const picBTN = document.getElementById("picCopy");
-const wifiBTN = document.getElementById("wifiCopy");
-const sidBTN = document.getElementById("sidCopy");
-const wpaBTN = document.getElementById("wpaCopy");
-
+const ids = ['pic', 'rid', 'email', 'wpa', 'zipcode', 'streetAddress', 'wifi'];
 const clearValues = document.getElementById("clear");
+const inputs = document.getElementsByName("inputBox");
+const copyBTNs = document.getElementsByName("copyBTN");
 
 document.addEventListener('DOMContentLoaded', init(), false);
 
-function setValue(key){
-    let keyValue = localStorage.getItem(key);
-    if(keyValue != null){
-        return keyValue;
-    }
-    else{
-        return "";
-    }
-}
-
 function init(){
-    picBox.value = setValue('picID');
-    wifiBox.value = setValue('wifiPassword');
-    wpaBox.value = setValue('wpaPassword');
-    sidBox.value = setValue('sidID');
+    ids.forEach(elementID => {
+        let element = document.getElementById(elementID);
+        if(localStorage.getItem(elementID) != null){
+            element.value = localStorage.getItem(elementID);
+        }
+    })
 }
 
-picBox.onblur = function(){
-    textLeave('picID', picBox.value);
-};
-wifiBox.onblur = function(){
-    textLeave('wifiPassword', wifiBox.value);
-};
-sidBox.onblur = function(){
-    textLeave('sidID', sidBox.value);
-};
-wpaBox.onblur = function(){
-    textLeave('wpaPassword', wpaBox.value);
-};
+inputs.forEach(input => {
+    input.onblur = function(){
+        localStorage.setItem(input.id, input.value);
+    }
+})
 
-function textLeave(itemKey, val){
-    localStorage.setItem(itemKey, val);
-}
-
-picBTN.onclick = function(){
-    copyText(picBox);
-}
-
-wifiBTN.onclick = function(){
-    copyText(wifiBox);
-}
-
-sidBTN.onclick = function(){
-    copyText(sidBox);
-}
-
-wpaBTN.onclick = function(){
-    copyText(wpaBox);
-}
-
-function copyText(node){
-    node.select();
-    node.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-}
+copyBTNs.forEach(btn => {
+    btn.onclick = function(){
+        let node = btn.previousElementSibling;
+        node.select();
+        node.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+})
 
 clearValues.onclick = function(){
-    picBox.value = '';
-    wpaBox.value = '';
-    wifiBox.value = '';
-    sidBox.value = '';
-    localStorage.removeItem('picID');
-    localStorage.removeItem('sidID');
-    localStorage.removeItem('wpaPassword');
-    localStorage.removeItem('wifiPassword');
+    inputs.forEach(input => {
+        input.value = '';
+    })
+    localStorage.clear();
 }
